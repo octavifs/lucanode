a1c2cff54f0692764513515ccb321b076d7c5c42
Octavi Font Sola <octavi.fs@gmail.com>
2018-01-31 16:58:44 +0100

    Added evaluation code

--- lucanode/training/detection.py
+++ lucanode/training/detection.py
@@ -43,5 +43,32 @@ def train(
     return model
 
 
-def predict():
-    pass
+def evaluate(
+    input_weights_file: str,
+    img_shape: Tuple[int, int],
+    ct_scans: Tuple[str, Callable[[str], str]],
+    lung_masks: Tuple[str, Callable[[str], str]],
+    nodule_masks: Tuple[str, Callable[[str], str]],
+):
+    "Train the network from scratch or from a preexisting set of weights on the dataset"
+    imgs_train = []
+    imgs_mask_train = []
+    ct = 0
+    for img_train, img_mask_train in loader.slices_with_nodules(ct_scans, lung_masks, nodule_masks):
+        ct += 1
+        if ct >= 10:
+            break
+        imgs_train.append(img_train[:, :, np.newaxis])
+        imgs_mask_train.append(img_mask_train[:, :, np.newaxis])
+
+    imgs_train = np.array(imgs_train)
+    imgs_mask_train = np.array(imgs_mask_train)
+    
+    model = Unet(*img_shape)
+    model.load_weights(input_weights_file, by_name=True)
+    return model.evaluate(
+        x=imgs_train,
+        y=imgs_mask_train,
+        batch_size=4,
+        verbose=1
+    )
