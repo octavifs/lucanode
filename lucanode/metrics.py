a99ff940ca48662327711c1e338cae96febe1d58
Octavi Font Sola <octavi.fs@gmail.com>
2018-03-19 12:21:20 +0100

    Changed 3ch dice coef to take into account the pre and post channel
    The dice coef will weight both pre and post at 25% each, while current
    slice keeps most of the weight (50%).
    
    The idea is that the segmentation is not only influenced by the current
    slice but also by the ones nearby, and as such it should be taken into
    account.

--- lucanode/metrics.py
+++ lucanode/metrics.py
@@ -48,7 +48,11 @@ def dice_coef_loss(y_true, y_pred):
 
 
 def dice_coef_3ch(y_true, y_pred, smooth=1):
-    y_true_f = K.flatten(y_true[:, :, :, 1])  # Only interested in mid channel, which contains current slice
+    y_true_f = (
+       K.flatten(y_true[:, :, :, 0]) / 4 +  # pre-channel has 25% incidence
+       K.flatten(y_true[:, :, :, 2]) / 4 +  # post-channel has 25% incidence
+       K.flatten(y_true[:, :, :, 1]) / 2    # current channel has 50% incidence
+    )
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
