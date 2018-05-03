d6461b0db2fdc97e05bf19b47736eb219221dd64
Octavi Font Sola <octavi.fs@gmail.com>
2018-05-03 18:32:59 +0200

    Removed artificial limitation of only checking 2 scans per subset (just added that to test the functionality quicker)

--- evaluate_lung_scan_segmentation.py
+++ evaluate_lung_scan_segmentation.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     with h5py.File(args.dataset, "r") as dataset:
         df = loader.dataset_metadata_as_dataframe(dataset)
         df = df[df.subset == args.subset]
-        scan_ids = list(set(df.seriesuid))[-2:]
+        scan_ids = set(df.seriesuid)
         metrics = []
         for seriesuid in tqdm(scan_ids):
             df_view = df[df.seriesuid == seriesuid]
