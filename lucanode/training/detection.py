26ed8aca43fe36add2aa5215a5be278a822fc103
Octavi Font Sola <octavi.fs@gmail.com>
2018-05-20 02:25:14 +0200

    Since before it was enough to not go out of bounds, just fix the vol_coords offset and go with smaller volumes

--- lucanode/training/detection.py
+++ lucanode/training/detection.py
@@ -716,11 +716,11 @@ def load_cubes(df, dataset, cube_size=32):
     for _, row in tqdm(df.iterrows(), desc="Loading cubes into memory", total=len(df)):
         if row.seriesuid != seriesuid:
             seriesuid = row.seriesuid
-            ct_scan_shape_aug = np.array(dataset["ct_scans"][row.seriesuid].shape) + (cube_side*2)
+            ct_scan_shape_aug = np.array(dataset["ct_scans"][row.seriesuid].shape) + cube_side
             ct_scan = augmentation.crop_to_shape(dataset["ct_scans"][row.seriesuid], ct_scan_shape_aug)
         world_coords = np.array([row.coordX, row.coordY, row.coordZ])
         world_origin = np.array(dataset["ct_scans"][row.seriesuid].attrs["origin"])
-        vol_coords = np.round(world_coords - world_origin).astype(np.int)[::-1] + cube_side
+        vol_coords = np.round(world_coords - world_origin).astype(np.int)[::-1] + cube_side // 2
         z_min = vol_coords[0] - cube_side
         z_max = vol_coords[0] + cube_side
         y_min = vol_coords[1] - cube_side
